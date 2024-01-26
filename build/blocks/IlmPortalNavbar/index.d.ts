26a3195cecd884796d16b17fa23f772d5a949105
asad@geniusmindzone.com <Asad Iqbal>
2024-01-26 11:18:00 +0500

    update code

---
+++ build/blocks/IlmPortalNavbar/index.d.ts
@@ -0,0 +1,32 @@
+import { MouseEventHandler } from "react";
+interface SidebarUri {
+    path: string;
+    icon?: string;
+    name: string;
+}
+export declare const IlmPortalNavbar: ({ onPortalChange, auth, logout, toggleMenu, menuState, orgs, onChangeOrgName, onChangeOrg, onSwitchOrg, sidebarUris, }: {
+    auth?: {
+        currentUser: {
+            photoURL: string;
+            displayName: string;
+            email: string;
+        };
+    };
+    logout?: MouseEventHandler<HTMLElement>;
+    menuState: Boolean;
+    toggleMenu?: () => void;
+    onPortalChange?: (portal: string) => void;
+    orgs?: Array<{
+        name: string;
+        displayName: string;
+        createdAt: string;
+        branding: {
+            logo_url: string;
+        };
+    }> | null;
+    onChangeOrgName?: (org: string) => void;
+    onChangeOrg?: (org: Object) => void;
+    onSwitchOrg?: () => void;
+    sidebarUris?: Array<SidebarUri>;
+}) => JSX.Element;
+export {};
---
+++ types/ilm-portal-nav.d.ts
@@ -0,0 +1,25 @@
+import React, { MouseEventHandler } from "react";
+
+interface SidebarUri {
+  path: string;
+  icon?: string;
+  name: string;
+}
+// Declaration file for types and exports from npm-package
+declare module "ilm-portal-nav" {
+  interface IlmPortalNavbarProps {
+    urls?: Array<{ name: string; path: string; icon?:string }> | null;
+    auth?: { currentUser: { photoURL: string; displayName: string; email: string } };
+    logout?: MouseEventHandler<HTMLElement>;
+    menuState: Boolean;
+    toggleMenu?: () => void;
+    onPortalChange?: (portal: string) => void;
+    orgs?: Array<{ name: string; displayName: string; createdAt: string; branding: { logo_url: string } }> | null;
+    onChangeOrgName?: (org: string) => void;
+    onChangeOrg?: (org: Object) => void;
+    onSwitchOrg?: () => void;
+    sidebarUris?: Array<SidebarUri>;
+  }
+
+  export const IlmPortalNavbar: React.FC<IlmPortalNavbarProps>;
+}
